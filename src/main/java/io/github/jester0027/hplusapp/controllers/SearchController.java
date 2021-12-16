c451fca8284379d9de5db0f7b8848a1b1b1df301
Paul <paul.ned@outlook.com>
2021-12-16 20:45:50 +0100

    search query

--- src/main/java/io/github/jester0027/hplusapp/controllers/SearchController.java
+++ src/main/java/io/github/jester0027/hplusapp/controllers/SearchController.java
@@ -1,16 +1,27 @@
 package io.github.jester0027.hplusapp.controllers;
 
+import io.github.jester0027.hplusapp.beans.Product;
+import io.github.jester0027.hplusapp.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
+
 @Controller
 public class SearchController {
 
+    @Autowired
+    private ProductRepository productRepository;
+
     @GetMapping("/search")
     public String search(@RequestParam("search") String search, Model model) {
-        System.out.println(search);
+        List<Product> products = productRepository.searchByName(search);
+        model.addAttribute("products", products);
+        products.forEach(System.out::println);
+        System.out.println(products.size());
         return "search";
     }
 }
