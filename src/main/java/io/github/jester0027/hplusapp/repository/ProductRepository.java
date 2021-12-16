229af508e428cde052e3f365dcd8d2c2c32160f9
Paul <paul.ned@outlook.com>
2021-12-16 20:17:11 +0100

    product repo searchByName method

--- src/main/java/io/github/jester0027/hplusapp/repository/ProductRepository.java
+++ src/main/java/io/github/jester0027/hplusapp/repository/ProductRepository.java
@@ -1,9 +1,15 @@
 package io.github.jester0027.hplusapp.repository;
 
 import io.github.jester0027.hplusapp.beans.Product;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ProductRepository extends CrudRepository<Product, Integer> {
+    @Query("SELECT p FROM Product p WHERE p.name like %:name%")
+    public List<Product> searchByName(@Param("name") String name);
 }
