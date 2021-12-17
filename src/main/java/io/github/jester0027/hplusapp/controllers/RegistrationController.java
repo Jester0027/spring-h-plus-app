bd59516cacd560ef8f2a11467ab4188623ca3c49
Paul <paul.ned@outlook.com>
2021-12-17 18:32:10 +0100

    date init binder

--- src/main/java/io/github/jester0027/hplusapp/controllers/RegistrationController.java
+++ src/main/java/io/github/jester0027/hplusapp/controllers/RegistrationController.java
@@ -3,13 +3,18 @@ package io.github.jester0027.hplusapp.controllers;
 import io.github.jester0027.hplusapp.beans.User;
 import io.github.jester0027.hplusapp.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @Controller
 public class RegistrationController {
@@ -17,8 +22,14 @@ public class RegistrationController {
     @Autowired
     private UserRepository userRepository;
 
+    @InitBinder
+    public void initBinder(WebDataBinder binder) {
+        binder.registerCustomEditor(Date.class, "dateOfBirth", new CustomDateEditor(new SimpleDateFormat("yyyy-MM-dd"), true));
+    }
+
     @PostMapping("/register")
     public String register(@Valid @ModelAttribute("newuser") User user, BindingResult bindingResult, Model model) {
+        System.out.println(user.getDateOfBirth());
         if (bindingResult.hasErrors()) {
             return "register";
         }
