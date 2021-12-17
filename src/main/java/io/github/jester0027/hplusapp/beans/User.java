ae479c314a27d8c37045fe56c61f2764be32c712
Paul <paul.ned@outlook.com>
2021-12-17 17:56:56 +0100

    bean validations

--- src/main/java/io/github/jester0027/hplusapp/beans/User.java
+++ src/main/java/io/github/jester0027/hplusapp/beans/User.java
@@ -2,16 +2,33 @@ package io.github.jester0027.hplusapp.beans;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 @Entity
 public class User {
     @Id
     private int id;
+
+    @Size(min = 6, message = "{username.min.six}")
     private String username;
+
+    @Pattern(
+            regexp = "((?=.*[A-Z].{6,10}))",
+            message = "Password must have one upper case, one lower case and a between 6 and 10 characters"
+    )
     private String password;
     private String gender;
+
+    @NotNull(message = "Activity cannot be empty")
     private String activity;
+
+    @NotEmpty(message = "First name cannot be empty")
     private String firstName;
+
+    @NotEmpty(message = "Last name cannot be empty")
     private String lastName;
     private String dateOfBirth;
 
--- src/main/java/io/github/jester0027/hplusapp/controllers/RegistrationController.java
+++ src/main/java/io/github/jester0027/hplusapp/controllers/RegistrationController.java
@@ -5,8 +5,11 @@ import io.github.jester0027.hplusapp.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @Controller
 public class RegistrationController {
@@ -15,7 +18,10 @@ public class RegistrationController {
     private UserRepository userRepository;
 
     @PostMapping("/register")
-    public String register(@ModelAttribute("newuser") User user, Model model) {
+    public String register(@Valid @ModelAttribute("newuser") User user, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            return "register";
+        }
         userRepository.save(user);
         model.addAttribute("dataSaved", "User registered successfully");
         return "login";
