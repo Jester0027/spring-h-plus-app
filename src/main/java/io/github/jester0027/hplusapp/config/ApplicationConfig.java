dbf1e8b9d5a87dd8471b15c6b37334c2372c65e3
Paul <paul.ned@outlook.com>
2021-12-18 12:40:34 +0100

    view resolvers

--- src/main/java/io/github/jester0027/hplusapp/config/ApplicationConfig.java
+++ src/main/java/io/github/jester0027/hplusapp/config/ApplicationConfig.java
@@ -4,14 +4,16 @@ import io.github.jester0027.hplusapp.converters.StringToEnumConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.task.AsyncTaskExecutor;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.web.servlet.config.annotation.AsyncSupportConfigurer;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
-import org.springframework.web.servlet.view.JstlView;
+import org.springframework.web.servlet.view.*;
+
+import java.util.Locale;
 
 @Configuration
 @ComponentScan(basePackages = "io.github.jester0027.hplusapp")
@@ -29,8 +31,25 @@ public class ApplicationConfig extends WebMvcConfigurationSupport {
         viewResolver.setPrefix("/WEB-INF/jsp/");
         viewResolver.setSuffix(".jsp");
         viewResolver.setViewClass(JstlView.class);
+        viewResolver.setOrder(2);
         return viewResolver;
     }
+
+    @Bean
+    public XmlViewResolver xmlViewResolver() {
+        XmlViewResolver viewResolver = new XmlViewResolver();
+        viewResolver.setLocation(new ClassPathResource("views.xml"));
+        viewResolver.setOrder(1);
+        return viewResolver;
+    }
+
+//    @Bean
+//    public ResourceBundleViewResolver resourceBundleViewResolver () {
+//        ResourceBundleViewResolver viewResolver = new ResourceBundleViewResolver();
+//        viewResolver.setBasename("views");
+//        return viewResolver;
+//    }
+
 
     @Override
     protected void addFormatters(FormatterRegistry registry) {
